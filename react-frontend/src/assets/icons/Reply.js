bd2980315255b67e10012f1288ac7a95ad87d694
Menaka-codebridge <menaka@codebridge.my>
2024-11-19 20:01:49 +0530

    CodeBridge commit

---
+++ react-frontend/src/assets/icons/Reply.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+const Checkline = (props) => (
+	<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" {...props} xmlns="http://www.w3.org/2000/svg">
+<path fill="currentColor" d="M9.24414 6.43476V4.32942C9.24414 3.15096 7.89356 2.55511 7.10573 3.3893L1.36578 9.46698C0.878073 9.98338 0.878073 10.8176 1.36578 11.334L7.10573 17.4117C7.89356 18.2458 9.24414 17.6632 9.24414 16.4848V14.247C15.4968 14.247 19.8737 16.3656 23 21C21.7495 14.3794 17.9979 7.75888 9.24414 6.43476Z"/>
+</svg>
+
+);
+export default Checkline;
